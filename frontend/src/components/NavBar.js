39334c14e42c4b669f8fda3cea93f190b0c8b08a
aibektoroev <aibektoroev@gmail.com>
2021-10-29 18:11:58 +0600

    TEsted and fixed navbar mobile responsiveness

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,33 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import "./navbar.css";
 
 export default function NavBar(props) {
-  const navBarStyles = {
-    top: "0",
-    maxWidth: "1080px",
-    marginLeft: "auto",
-    marginRight: "auto",
-    backgroundColor: "#0269A4",
-    overflow: "hidden",
-  };
-
   const onLoginClicked = (event) => {
     event.preventDefault();
 
-    // implement login fuctionality
+    // TO DO : implement login fuctionality
 
     console.log("LOGIN Clicked!!!");
   };
 
   return (
-    <nav
-      className="navbar navbar-dark navbar-expand-lg fixed-top justify-content-between mb-2"
-      style={navBarStyles}
-    >
+    <nav className="navbar navbar-dark navbar-expand-lg fixed-top justify-content-between mb-2">
       <a className="navbar-brand ms-3 me-auto" href="/#">
         Справочник МКБ-10 для Кыргызстана
       </a>
-      <SearchBar searchItemClickedHandler={props.searchItemClickedHandler} />
+      <SearchBar
+        className="ms-3"
+        searchItemClickedHandler={props.searchItemClickedHandler}
+      />
       <a href="/#" onClick={onLoginClicked} className="text-light me-4 px-3">
         Войти
       </a>
